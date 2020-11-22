f5e02fcfab758dcc0958e120fe340c885a52198a
varunp2k <varunp2k@gmail.com>
2020-11-23 05:12:01 +0530

    Added hashing, encryption,decryption functions

--- ge-hack-be/index.js
+++ ge-hack-be/index.js
@@ -1,57 +1,83 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const FileReader = require("filereader");
+const CryptoJS = require("crypto-js")
+const { Transform } = require('stream');
 
-function encrypt(input) {
-    var file = input.files[0];
-    var reader = new FileReader();
-    reader.onload = () => {
-        var key = "1234567887654321";
-        var wordArray = CryptoJS.lib.WordArray.create(reader.result);           // Convert: ArrayBuffer -> WordArray
-        var encrypted = CryptoJS.AES.encrypt(wordArray, key).toString();        // Encryption: I: WordArray -> O: -> Base64 encoded string (OpenSSL-format)
+class AppendInitVect extends Transform {
+  constructor(initVect, opts) {
+    super(opts);
+    this.initVect = initVect;
+    this.appended = false;
+  }
 
-        var fileEnc = new Blob([encrypted]);                                    // Create blob from string
-
-        var a = document.createElement("a");
-        var url = window.URL.createObjectURL(fileEnc);
-        var filename = file.name + ".enc";
-        a.href = url;
-        a.download = filename;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    };
-    reader.readAsArrayBuffer(file);
-}
-
-function convertWordArrayToUint8Array(wordArray) {
-    var arrayOfWords = wordArray.hasOwnProperty("words") ? wordArray.words : [];
-    var length = wordArray.hasOwnProperty("sigBytes") ? wordArray.sigBytes : arrayOfWords.length * 4;
-    var uInt8Array = new Uint8Array(length), index=0, word, i;
-    for (i=0; i<length; i++) {
-        word = arrayOfWords[i];
-        uInt8Array[index++] = word >> 24;
-        uInt8Array[index++] = (word >> 16) & 0xff;
-        uInt8Array[index++] = (word >> 8) & 0xff;
-        uInt8Array[index++] = word & 0xff;
+  _transform(chunk, encoding, cb) {
+    if (!this.appended) {
+      this.push(this.initVect);
+      this.appended = true;
     }
-    return uInt8Array;
+    this.push(chunk);
+    cb();
+  }
 }
+module.exports = AppendInitVect;
 
-function decrypt(input) {
-    var file = input.files[0];
-    var reader = new FileReader();
-    reader.onload = () => {
-        var key = "1234567887654321";  
+function getCipherKey(password) {
+    return crypto.createHash('sha256').update(password).digest();
+  }
 
-        var decrypted = CryptoJS.AES.decrypt(reader.result, key);               // Decryption: I: Base64 encoded string (OpenSSL-format) -> O: WordArray
-        var typedArray = convertWordArrayToUint8Array(decrypted);               // Convert: WordArray -> typed array
-
-        var fileDec = new Blob([typedArray]);                                   // Create blob from typed array
-
-        var a = document.createElement("a");
-        var url = window.URL.createObjectURL(fileDec);
-        var filename = file.name.substr(0, file.name.length - 4) + ".dec";
-        a.href = url;
-        a.download = filename;
-        a.click();
-        window.URL.revokeObjectURL(url);
-    };
-    reader.readAsText(file);
+function hashFile(file){
+    const readStream = fs.createReadStream(file);
+    var wordArray = CryptoJS.lib.WordArray.create(readStream);
+    var hash = CryptoJS.SHA256(wordArray);
+    var hashed = hash.toString();
+    console.log(hashed);
 }
+// hashFile('./PES1201800326_Week_10-11_OS_Lab.pdf');
+
+function encrypt( {file, password} ) {
+    // Generate a secure, pseudo random initialization vector.
+    const initVect = crypto.randomBytes(16);
+    
+    // Generate a cipher key from the password.
+    const CIPHER_KEY = getCipherKey(password);
+    const readStream = fs.createReadStream(file);
+    const gzip = zlib.createGzip();
+    const cipher = crypto.createCipheriv('aes256', CIPHER_KEY, initVect);
+    const appendInitVect = new AppendInitVect(initVect);
+    // Create a write stream with a different file extension.
+    const writeStream = fs.createWriteStream(path.join(file + ".enc"));
+    
+    readStream
+      .pipe(gzip)
+      .pipe(cipher)
+      .pipe(appendInitVect)
+      .pipe(writeStream);
+  }
+
+// encrypt({ file: './PES1201800326_Week_10-11_OS_Lab.pdf', password: 'userId' });
+
+function decrypt({ file, password }) {
+    // First, get the initialization vector from the file.
+    const readInitVect = fs.createReadStream(file, { end: 15 });
+  
+    let initVect;
+    readInitVect.on('data', (chunk) => {
+      initVect = chunk;
+    });
+    // Once we’ve got the initialization vector, we can decrypt the file.
+    readInitVect.on('close', () => {
+      const cipherKey = getCipherKey(password);
+      const readStream = fs.createReadStream(file, { start: 16 });
+      const decipher = crypto.createDecipheriv('aes256', cipherKey, initVect);
+      const unzip = zlib.createUnzip();
+      const writeStream = fs.createWriteStream(file + '.unenc');
+  
+      readStream
+        .pipe(decipher)
+        .pipe(unzip)
+        .pipe(writeStream);
+    });
+  }
