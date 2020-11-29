82360357d0dcda37bec9d0ce61d6fa3916cb4ac8
NaikAayush <ayush@pesu.pes.edu>
2020-11-29 14:15:29 +0530

    Added User mRecord View

--- ge-hack-backend/functions/index.js
+++ ge-hack-backend/functions/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const bc = require('./blockchain');
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const bc = require("./blockchain");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 //initialize express server
 const app = express();
@@ -13,9 +13,11 @@ const main = express();
 main.use("/api/v1", app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload({
-  debug: true
-}));
+app.use(
+  fileUpload({
+    debug: true,
+  })
+);
 // app.use(decodeIDToken);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -41,28 +43,28 @@ const dCenterCollection = db.collection("dCenter");
 //define google cloud function name
 exports.webApi = functions.https.onRequest(main);
 
-
- // Create and Deploy Your First Cloud Functions
- // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
 
-
-
 /**
  * Decodes the JSON Web Token sent via the frontend app
  * Makes the currentUser (firebase) data available on the body.
  */
 async function decodeIDToken(req, res) {
-  if ("authorization" in req.headers && req.headers.authorization.startsWith('Bearer ')) {
-    const idToken = req.headers.authorization.split('Bearer ')[1];
+  if (
+    "authorization" in req.headers &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
 
     try {
       const decodedToken = await admin.auth().verifyIdToken(idToken);
-      req['currentUser'] = decodedToken;
+      req["currentUser"] = decodedToken;
     } catch (err) {
       res.status(401).send(err);
     }
@@ -86,10 +88,20 @@ app.get("/hospital/get/:id", async (req, res) => {
   await bc.getBC(`/Hospital/${req.params.id}`, req, res);
 });
 app.get("/hospital/medicalRecord/", async (req, res) => {
-  await bc.queryBCOneParam("hospital", "HospitalAccessedMedicalRecords", req, res);
+  await bc.queryBCOneParam(
+    "hospital",
+    "HospitalAccessedMedicalRecords",
+    req,
+    res
+  );
 });
 app.get("/hospital/insuranceRecord/", async (req, res) => {
-  await bc.queryBCOneParam("hospital", "HospitalAccessedInsuranceRecords", req, res);
+  await bc.queryBCOneParam(
+    "hospital",
+    "HospitalAccessedInsuranceRecords",
+    req,
+    res
+  );
 });
 app.get("/hospital/invoice/", async (req, res) => {
   await bc.queryBCOneParam("hospital", "HospitalInvoices", req, res);
@@ -110,10 +122,20 @@ app.get("/provider/get/:id", async (req, res) => {
   await bc.getBC(`/InsuranceProvider/${req.params.id}`, req, res);
 });
 app.get("/provider/medicalRecord/", async (req, res) => {
-  await bc.queryBCOneParam("iProvider", "IProviderAccessedMedicalRecords", req, res);
+  await bc.queryBCOneParam(
+    "iProvider",
+    "IProviderAccessedMedicalRecords",
+    req,
+    res
+  );
 });
 app.get("/provider/insuranceRecord/", async (req, res) => {
-  await bc.queryBCOneParam("iProvider", "IProviderAccessedInsuranceRecords", req, res);
+  await bc.queryBCOneParam(
+    "iProvider",
+    "IProviderAccessedInsuranceRecords",
+    req,
+    res
+  );
 });
 app.post("/provider/insuranceRecord/", async (req, res) => {
   await bc.addInsuranceRecord(req, res);
@@ -126,18 +148,46 @@ app.get("/dCenter/get/:id", async (req, res) => {
   await bc.getBC(`/DiagCenter/${req.params.id}`, req, res);
 });
 app.get("/dCenter/medicalRecord/", async (req, res) => {
-  await bc.queryBCOneParam("dCenter", "DCenterAccessedMedicalRecords", req, res);
+  await bc.queryBCOneParam(
+    "dCenter",
+    "DCenterAccessedMedicalRecords",
+    req,
+    res
+  );
 });
 app.post("/dCenter/medicalRecord/", async (req, res) => {
   await bc.addMedicalRecord("dCenter", req, res);
 });
 
+//USER VIEWS
 app.get("/user/get/:id", async (req, res) => {
-  await bc.getBC(`/User/${req.params.id}`, req, res);
+  const doc = await db.collection("users").doc(req.params.id).get();
+  if (!doc.exists) {
+    console.log("User does not exist!");
+  } else {
+    res.send(doc.data());
+  }
+  // await bc.getBC(`/User/${req.params.id}`, req, res);
 });
-app.get("/user/medicalRecord/", async (req, res) => {
-  await bc.queryBCOneParam("user", "UserMedicalRecords", req, res);
+
+// app.get("/user/get/:id", async (req, res) => {
+//   await bc.getBC(`/User/${req.params.id}`, req, res);
+// });
+app.get("/user/views/medicalRecord/:userId", async (req, res) => {
+  const userRef = db.collection("medicalRecord");
+  const snapshot = await userRef.where("uid", "==", req.params.userId).get();
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+  snapshot.forEach((doc) => {
+    res.send({ id: doc.id, data: doc.data() });
+  });
+  // await bc.queryBCOneParam("user", "UserMedicalRecords", req, res);
 });
+// app.get("/user/medicalRecord/", async (req, res) => {
+//   await bc.queryBCOneParam("user", "UserMedicalRecords", req, res);
+// });
 app.get("/user/insuranceRecord/", async (req, res) => {
   await bc.queryBCOneParam("user", "UserInsuranceRecords", req, res);
 });
@@ -147,22 +197,21 @@ app.get("/user/invoice/", async (req, res) => {
 app.get("/user/getUid/", async (req, res) => {
   if ("phoneNumber" in req.query) {
     admin
-    .auth()
-    .getUserByPhoneNumber("+" + req.query.phoneNumber)
-    .then((userRecord) => {
-      console.log(`Successfully fetched user data:  ${userRecord.toJSON()}`);
-      res.status(200).send(userRecord);
-      return true;
-    })
-    .catch((error) => {
-      console.log('Error fetching user data:', error);
-      res.status(500).send(error);
-    });
+      .auth()
+      .getUserByPhoneNumber("+" + req.query.phoneNumber)
+      .then((userRecord) => {
+        console.log(`Successfully fetched user data:  ${userRecord.toJSON()}`);
+        res.status(200).send(userRecord);
+        return true;
+      })
+      .catch((error) => {
+        console.log("Error fetching user data:", error);
+        res.status(500).send(error);
+      });
   } else {
     res.status(400).send("Not enough parameters");
   }
 });
-
 
 // admin
 app.get("/admin/hospital", async (req, res) => {
@@ -175,17 +224,17 @@ app.get("/admin/getCustomToken", async (req, res) => {
     return;
   }
   admin
-  .auth()
-  .createCustomToken(req.query.uid)
-  .then((customToken) => {
-    // Send token back to client
-    res.status(200).send(customToken);
-    return;
-  })
-  .catch((error) => {
-    console.log('Error creating custom token:', error);
-    res.status(500).send(error);
-  });
+    .auth()
+    .createCustomToken(req.query.uid)
+    .then((customToken) => {
+      // Send token back to client
+      res.status(200).send(customToken);
+      return;
+    })
+    .catch((error) => {
+      console.log("Error creating custom token:", error);
+      res.status(500).send(error);
+    });
 });
 
 app.post("/admin/hospital", async (req, res) => {
@@ -207,7 +256,6 @@ app.get("/admin/iProvider", async (req, res) => {
 app.post("/admin/iProvider", async (req, res) => {
   await bc.createUser("InsuranceProvider", providerCollection, "i", req, res);
 });
-
 
 // medical record access
 async function grantAccess(req, res) {
